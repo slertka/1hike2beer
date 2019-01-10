05c0a16a4079fba942856644e76920e9b10512a8
Serena Lertkantitham <lertkantitham@gmail.com>
2019-01-09 20:48:13 -0800

    display beer results with hike results!

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 // API KEYS
 const hikeAPIKey = '200405472-b58f2be71509e5cbbac487a794df0b61';
 const mapsAPIKey = 'AIzaSyA2pbng72aHFW9jfZ7wmXT8H12MNpTerW8';
-var coordinates = {
-  "lat": 47.6062095,
-  "lng": -122.3320708
-};
 
 // BASE API URLS
 const hikeURL = 'https://www.hikingproject.com/data/get-trails?';
-const yelpURL = 'https://api.yelp.com/v3/businesses/search?categories=breweries,all&';
+const beerURL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=brewery&';
 const geocodeURL = 'https://maps.googleapis.com/maps/api/geocode/json?';
 
 // Function converts location entered in search to lattitude and longitude
@@ -26,6 +22,7 @@ function convertLocToLatLong(location, dist){
     .then(responseJSON => {
       let searchCoord = responseJSON.results[0].geometry.location;
       getHikeResults(searchCoord, dist);
+      initPlaceMap(searchCoord, dist);
     })
     .catch(err => console.log(err.message))
 }
@@ -56,6 +53,34 @@ function getHikeResults(coordinates, dist){
       throw new Error(response.statusText);
     })
     .then(responseJSON => displayHikeResults(responseJSON))
+}
+
+function displayBeerResults(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) { 
+    $('.js-beer-results').empty();
+    for (let i=0; i<results.length; i++) {
+      $('.js-beer-results').append(`
+        <li>
+          <h4>${results[i].name}</h4>
+          <p>Rating: ${results[i].rating}/5 stars based on ${results[i].user_ratings_total}</p>
+        </li>
+      `)
+    }
+  }
+}
+
+function initPlaceMap(coordinates, dist) {
+  let searchArea = new google.maps.LatLng(coordinates.lat, coordinates.lng);
+  let map = new google.maps.Map(document.getElementById('map'));
+
+  var request = {
+    location: searchArea,
+    radius: dist*1609,
+    keyword: 'brewery'
+  };
+
+  let service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, displayBeerResults)
 }
 
 function watchForm(){
